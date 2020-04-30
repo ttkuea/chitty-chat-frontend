b859843c3ddfe1091657ff9ae8c5f1a8b6de3947
Rodchananat K <rod8711@gmail.com>
2020-04-30 12:08:53 +0700

    add unread marker

--- src/views/chat/chat-room/chat-room.js
+++ src/views/chat/chat-room/chat-room.js
@@ -4,6 +4,7 @@ import { Repeat } from '../chat';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ChatBubble from './components/chat-bubble';
+import UnreadMarker from './components/unread-marker';
 
 const fakeMessage = {
     message: "doge doge",
@@ -19,12 +20,14 @@ const ChatRoom = ({groupName}) => {
             </div>
             <div className="chat">
                 <Repeat count={2}>
-                <ChatBubble message={fakeMessage} isOwn={false}/>
-                <ChatBubble message={fakeMessage} isOwn={false}/>
+                    <ChatBubble message={fakeMessage} isOwn={false}/>
+                    <ChatBubble message={fakeMessage} isOwn={false}/>
+                    <ChatBubble message={fakeMessage} isOwn={false}/>
+                    <ChatBubble message={fakeMessage} isOwn={true}/>
+                </Repeat>
+                <UnreadMarker/>
                 <ChatBubble message={fakeMessage} isOwn={false}/>
                 <ChatBubble message={fakeMessage} isOwn={true}/>
-                    
-                </Repeat>
             </div>
             <div className="chat-box">
                 <div className="input" placeholder="Enter a message." contentEditable></div>
---
+++ src/views/chat/chat-room/components/unread-marker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './unread-marker.scss';
+
+const UnreadMarker = () => {
+    return (
+        <div className="unread-marker"> 
+            unread messages below
+        </div>
+    )
+}
+
+export default UnreadMarker;
