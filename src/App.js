4fd8c96c574063307497f6f86987cc3ecbc4e343
kanjeranian <oilscius@gmail.com>
2020-04-30 22:33:17 +0700

    feat/login finisn with no api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import request from 'superagent';
+import Register from './register';
+import Login from './login';
+import NotFoundPage from './not-found';
+import styled from 'styled-components';
 
-function App() {
+const Page = styled.div`
+  margin: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 1547px;
+  height: 100vh;
+  background-color: #a3d1d3;
+  color: black;
+`;
+const Head = styled.div`
+  background-color: #323232;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 32px;
+`;
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page>
+      <Head>CHITTY CHAT</Head>
+      <Router>
+        <Switch>
+          <Route exact path='/register'>
+            <Register />
+          </Route>
+          <Route exact path='/login'>
+            <Login />
+          </Route>
+          <Route path=''>
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </Page>
   );
-}
+};
 
-export default App;
+export default () => <App />;
+// function App() {
+//   return (
+//     <div className='App'>
+//       <div class='container'>
+//         <div class='row'>
+//           <div class='col-sm'>One of three columns</div>
+//           <div class='col-sm'>One of three columns</div>
+//           <div class='col-sm'>One of three columns</div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
