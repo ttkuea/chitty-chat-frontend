4e0adc8e0280d39fc4cd630acc16ddda3b7500e9
Flap <flap.atyz@gmail.com>
2020-05-01 20:32:11 +0700

    store try to use ramda clone

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
 // global and only store
 import React, {createContext, useReducer} from 'react';
 import socketIOClient from 'socket.io-client';
+import { clone } from 'ramda';
 
 const ENDPOINT = process.env.ENDPOINT || "http://localhost:3000";
 const socket = window.gsocket = socketIOClient(ENDPOINT); // tolerate reload 
 
 const initialState = {  loginUsername: 'testuser1', //TODO change to null
-                        groups: [] };
+    loginId: "5eaba24e1604a553c9c4e7e6" };
 const store = createContext(initialState);
 const { Provider } = store;
 
@@ -15,22 +16,22 @@ const StateProvider = ( { children } ) => {
     const [state, dispatch] = useReducer((state, action) => {
         switch(action.type) {
             case 'set':
+                console.log('set', action.newState)
+                const newState = {
+                    ...state,
+                    ...clone(action.newState)
+                };
+                return newState;
+            case 'setGroup':
                 return {
                     ...state,
-                    ...action.newState
+                    ...clone(action.groups)
                 }
-            case 'login':
-
-                return {
-                    ...state,
-                    loginUsername: action.payload
-                }
-
             default:
                 throw new Error();
         ;}
 
-    },initialState);
+    }, initialState);
 
     return <Provider value={{ state, dispatch }}>{children}</Provider>;
 };
